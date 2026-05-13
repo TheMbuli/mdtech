286ad4d76457d1d3b71dc82f19ef993c392882b9
EDDY MBULINYOLO <thembledpro@gmail.com>
2026-05-13 20:59:27 +0200

    add vercel json

--- mdtech/settings.py
+++ mdtech/settings.py
@@ -136,6 +136,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
+STATICFILES_DIR = [
+    BASE_DIR / "static"
+]
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
